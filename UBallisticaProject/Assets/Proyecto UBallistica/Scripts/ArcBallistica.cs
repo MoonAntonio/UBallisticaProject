eb54c872be2dd77246e66bcbed281c60942564b7
Moon Antonio <pinchodev91@gmail.com>
2017-03-31 01:23:15 +0200

    Preparando escena

---
+++ UBallisticaProject/Assets/Proyecto UBallistica/Scripts/ArcBallistica.cs
@@ -0,0 +1,23 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// ArcBallistica.cs (30/03/2017)												\\
+// Autor: Antonio Mateo (Moon Antonio) 									        \\
+// Descripcion:		Generador del arco de la ballistica							\\
+// Fecha Mod:		30/03/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio
+{
+	/// <summary>
+	/// <para>Generador del arco de la ballistica</para>
+	/// </summary>
+	public class ArcBallistica : MonoBehaviour 
+	{
+
+	}
+}
